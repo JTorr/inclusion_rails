42f5924f0488ba03fda33fd86e415f10377b30b7
Julie Torres <reeddetorres@gmail.com>
2014-09-03 17:00:30 -0400

    Add URI escape

--- app/models/gem_fetcher.rb
+++ app/models/gem_fetcher.rb
@@ -1,7 +1,7 @@
 class GemFetcher
   attr_accessor :response
   def self.fetch(ruby_gem)
-    @response = HTTParty.get("https://rubygems.org/api/v1/search.json?query=#{ruby_gem}")
+    @response = HTTParty.get(URI.escape("https://rubygems.org/api/v1/search.json?query=#{ruby_gem}"))
   end
 
 end
