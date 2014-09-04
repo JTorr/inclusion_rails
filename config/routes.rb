5f0a9c82ed6f1cd9bae3678b4ddfb7fdb0726a2d
Julie Torres <reeddetorres@gmail.com>
2014-09-04 11:33:27 -0400

    Add route and view for favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'favorites/index'
-
   resources :gem_fetchers, only: [:index, :new, :create, :show]
+  resources :favorites, only: [:index]
   devise_for :users
   root to: "home#index"
+
+  #   resources :jobs do
+  #   resource :favorites, only: [:create, :destroy]
+  # end
 end
