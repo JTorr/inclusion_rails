3b88dd384acebab2dabb2313b2dee4dc10b2055c
Robert Groller <robertgroller@gmail.com>
2014-09-06 10:30:05 -0400

    Remove duplicate join table migration

--- db/migrate/20140906140708_create_join_table_project_gems.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableProjectGems < ActiveRecord::Migration
-  def change
-    create_join_table :Projects, :RubyGems do |t|
-      t.index [:project_id, :ruby_gem_id]
-      t.index [:ruby_gem_id, :project_id]
-    end
-  end
-end
