3375ce44c77e2a2b52fe7686bb212fd3c826a643
Robert Groller <robertgroller@gmail.com>
2014-09-07 12:37:44 -0400

    Fix attach_gem and attach_gems methods

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   belongs_to :user
   has_many :project_gems
   has_many :ruby_gems, through: :project_gems
-  validates_uniqueness_of :project_gems
+  #check for uniqueness of a project_gem
 
   def parse(string)
     @gem_list = []
@@ -15,32 +15,45 @@ class Project < ActiveRecord::Base
   end
 
   def attach_gem(gem)
-    @gem_data =  GemApi.fetch gem
+    gem_data =  GemApi.fetch gem
+    
+    if gem_data.ok? # ok means returned code 200
+      gem_name = gem_data["name"]
+      gem_info = gem_data["info"]
 
-    if @gem_data.ok?
-      @gem_name = @gem_data["name"]
-      @gem_info = @gem_data["info"]
-      if RubyGem.in_db?(@gem_name)
-        @gem_model = RubyGem.in_db @gem_name
-        @gem_model.update(name: @gem_name, info: @gem_info)
-        self.ruby_gems << @gem_model
-        self.save
+      if RubyGem.in_db? gem_name
+        gem_model = RubyGem.in_db gem_name
+        gem_model.update(name: gem_name, info: gem_info)
+        
+        # unless the gem is already included in the project, add it
+        unless self.ruby_gems.include? gem_model
+          self.ruby_gems << gem_model
+          self.save
+        end
+          
       else
-        @gem_model = self.ruby_gems << RubyGem.create(name: @gem_name, info: @gem_info)
+        gem_model = self.ruby_gems << RubyGem.create(name: gem_name, info: gem_info)
         self.save
       end
 
     else
-      if RubyGem.in_db?(@gem_name)
-        @gem_model = RubyGem.in_db @gem_name
-        self.ruby_gems << @gem_model
+      if RubyGem.in_db? gem_name
+        gem_model = RubyGem.in_db gem_name
+        
+        # unless the gem is already included in the project, add it
+        unless self.ruby_gems.include? gem_model
+          self.ruby_gems << gem_model
+          self.save
+        end
+
       else
-        raise ApiError, "The ruby gems api raised the following code #{@gem_data.code}."
+        raise "The ruby gems api raised the following code: #{gem_data.code}."
       end
     end
   end
 
   def attach_gems
+    self.ruby_gems.delete_all
     self.save
     @gems_not_found = []
     @gem_list.each do |gem|
