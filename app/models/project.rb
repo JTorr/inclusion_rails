4d047348306169824ef52722361b8fe8b31a9b99
Julie Torres <reeddetorres@gmail.com>
2014-09-22 21:58:02 -0400

    Finish refactoring project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,8 +2,8 @@ class Project < ActiveRecord::Base
   belongs_to :user
   has_many :project_gems
   has_many :ruby_gems, through: :project_gems
-  #check for uniqueness of a project_gem
 
+#parse gemfile, return individual gems as @gem_list
   def parse(string)
     @gem_list = []
     lines = string.split("\n")
@@ -15,42 +15,48 @@ class Project < ActiveRecord::Base
     @gem_list
   end
 
+# unless the gem is already in project, add it
   def ensure_gem_in_db name
     model = RubyGem.in_db name
 
-    # unless the gem is already included in the project, add it
     unless self.ruby_gems.include? model
       self.ruby_gems << model
       self.save
     end
-
     model
   end
 
   def attach_gem(gem, fetcher=nil)
+    #fetcher argument used for testing purposes
     fetcher ||= GemApi
     gem_data = fetcher.fetch gem
 
     if gem_data.ok? # ok means returned code 200
-      gem_name = gem_data["name"]
-      gem_info = gem_data["info"]
-
-      if RubyGem.in_db? gem_name
-        gem_model = ensure_gem_in_db gem_name
-        gem_model.update(name: gem_name, info: gem_info)
-
-      else
-        gem_model = self.ruby_gems << RubyGem.create(name: gem_name, info: gem_info)
-        self.save
-      end
-
+      handle_valid_api_response gem_data
     else
-      # gem_name = nil because it's from RubyGems
-      if RubyGem.in_db? gem
-        ensure_gem_in_db gem
-      else
-        raise "The ruby gems api raised the following code: #{gem_data.code}."
-      end
+      handle_api_error gem
+    end
+  end
+
+  def handle_valid_api_response gem_data
+    gem_name = gem_data["name"]
+    gem_info = gem_data["info"]
+
+    if RubyGem.in_db? gem_name
+      gem_model = ensure_gem_in_db gem_name
+      gem_model.update(name: gem_name, info: gem_info)
+    else
+      gem_model = self.ruby_gems << RubyGem.create(name: gem_name, info: gem_info)
+      self.save
+    end
+  end
+
+
+  def handle_api_error gem
+    if RubyGem.in_db? gem
+      ensure_gem_in_db gem
+    else
+      raise "The ruby gems api raised the following code: #{gem_data.code}."
     end
   end
 
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe Project, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can parse a Gemfile' do
+    project = Project.new
+    results = project.parse %{
+# Can bundle against edge rails
+source :rubygems
+
+gem 'rails'
+gem 'jquery', '~> 3.0'
+
+group :development do
+  gem 'spring'
+end
+    }
+    expect( results ).to eq %w( rails jquery spring )
+  end
+
+  it "won't double-parse" do
+    gemfile = %{ gem 'rails' }
+    project = Project.new
+    project.parse gemfile
+    results = project.parse gemfile
+    expect( results ).to eq ['rails']
+  end
+
+  it 'can create and attach a new gem' do
+    project = Project.new
+    project.attach_gem 'pry'
+
+    expect( project.ruby_gems.count ).to eq 1
+    expect( project.ruby_gems.first.name ).to eq 'pry'
+  end
+
+  it 'can update and attach an existing gem' do
+    old = RubyGem.create! name: 'pry', info: 'Old info'
+
+    project = Project.new
+    project.attach_gem 'pry'
+
+    expect( RubyGem.count ).to eq 1
+    pry = RubyGem.first
+
+    expect( pry.name ).to eq 'pry'
+    expect( pry.info ).not_to eq 'Old info'
+  end
+
+  it 'can find existing gems even if RubyGems is down' do
+    # Can use rspec-mock or mocha to clean this up
+    class MockGemFetcher
+      def self.fetch gem
+        BadResponse.new
+      end
+    end
+    
+    class BadResponse
+      def ok?
+        false
+      end
+      def code
+        500
+      end
+    end
+
+    old = RubyGem.create! name: 'pry', info: 'Old info'
+
+    project = Project.new
+    project.attach_gem 'pry', MockGemFetcher
+
+    expect( RubyGem.count ).to eq 1
+
+    pry = RubyGem.first
+    expect( pry.name ).to eq 'pry'
+    expect( pry.info ).to eq 'Old info'
+  end
 end
