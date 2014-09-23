19f276b5086a0ca21bfc461b16ae2d670287f295
Julie Torres <reeddetorres@gmail.com>
2014-09-22 21:28:29 -0400

    Begin refactoring project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,35 +12,32 @@ class Project < ActiveRecord::Base
         @gem_list.push line.scan(/[A-Za-z0-9_-]+/)[1]
       end
     end
+    @gem_list
   end
 
-def get_gem_info(gem)
-  gem_data =  GemApi.fetch gem
+  def ensure_gem_in_db name
+    model = RubyGem.in_db name
 
-  if gem_data.ok? # ok means returned code 200
-    @gem_name = gem_data["name"]
-    @gem_info = gem_data["info"]
+    # unless the gem is already included in the project, add it
+    unless self.ruby_gems.include? model
+      self.ruby_gems << model
+      self.save
+    end
+
+    model
   end
-end
 
-
-
-  def attach_gem(gem)
-    gem_data =  GemApi.fetch gem
+  def attach_gem(gem, fetcher=nil)
+    fetcher ||= GemApi
+    gem_data = fetcher.fetch gem
 
     if gem_data.ok? # ok means returned code 200
       gem_name = gem_data["name"]
       gem_info = gem_data["info"]
 
       if RubyGem.in_db? gem_name
-        gem_model = RubyGem.in_db gem_name
+        gem_model = ensure_gem_in_db gem_name
         gem_model.update(name: gem_name, info: gem_info)
-
-        # unless the gem is already included in the project, add it
-        unless self.ruby_gems.include? gem_model
-          self.ruby_gems << gem_model
-          self.save
-        end
 
       else
         gem_model = self.ruby_gems << RubyGem.create(name: gem_name, info: gem_info)
@@ -48,20 +45,14 @@ end
       end
 
     else
-      if RubyGem.in_db? gem_name
-        gem_model = RubyGem.in_db gem_name
-
-        # unless the gem is already included in the project, add it
-        unless self.ruby_gems.include? gem_model
-          self.ruby_gems << gem_model
-          self.save
-        end
-
-    else
-      raise "The ruby gems api raised the following code: #{gem_data.code}."
+      # gem_name = nil because it's from RubyGems
+      if RubyGem.in_db? gem
+        ensure_gem_in_db gem
+      else
+        raise "The ruby gems api raised the following code: #{gem_data.code}."
+      end
     end
   end
-end
 
   def attach_gems
     self.ruby_gems.delete_all
